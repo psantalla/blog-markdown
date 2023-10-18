d362f63d7a13556f01728fd98a628c828a6ff362
Pablo Santalla <pablosantalla92@gmail.com>
2023-10-18 01:52:06 -0700

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,13 +14,13 @@ const Footer = () => {
               href="https://nextjs.org/docs/basic-features/pages"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Site created by Pablo Santalla
             </a>
             <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              Go to portfolio website
             </a>
           </div>
         </div>
